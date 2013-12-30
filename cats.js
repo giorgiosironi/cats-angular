6bbf5a75e9edc5afcae50c24a33a88e79f972c2b
Giorgio Sironi <giorgio.sironi@neomobile.com>
2013-12-30 15:46:16 +0100

    Generating also the cat's name

--- cats.js
+++ cats.js
@@ -25,11 +25,14 @@ cats.factory("CatsList", function () {
     var adding = 2
 
     var generateName = function () {
-        return "Tom"
+        var names = ["Kitty", "Tiger", "Smokey", "Shadow", "Tigger", "Baby", "Princess", "Max", "Oreo", "Angel", "Bella", "Buddy", "Gizmo", "Midnight", "Sassy", "Patches", "Simba", "Precious", "Lucky", "Lucy", "Chloe", "Boots", "Charlie", "Callie", "Sammy", "Jack", "Pepper", "Molly", "Missy", "Kiki", "Fluffy", "Daisy", "Cleo", "Garfield", "Lily", "Cali", "Gracie", "Sophie", "Oliver", "Milo", "Toby", "Sam", "Pumpkin", "Jasper", "Sasha", "Misty", "Bailey", "Rocky", "Jasmine", "Felix", "Oscar", "Bandit", "Ginger", "Mittens", "Simon", "Peanut", "Coco", "Harley", "Lilly", "Boo", "Luna", "Cookie", "Abby", "Mimi", "Snowball", "Salem", "George", "Snickers", "Nala", "Sugar", "Leo", "Casper", "Miss kitty", "Maggie", "Trouble", "Rascal", "Cuddles", "Scooter", "Tinkerbell", "Samantha", "Buster", "Zoey", "Sadie", "Lola", "Bear", "Willow", "Spooky", "Dusty", "Zoe", "Mia", "Whiskers", "Bob", "Muffin", "Socks", "Chester", "Sheba", "Snuggles", "Loki", "Annie", "Jinx"]
+        return names[Math.floor(Math.random() * names.length)];
     }
+
     var generateImage = function () {
         return templateUrl + "&random=" + Math.floor(Math.random() * 1000)
     }
+
     var newCat = function() {
         return {
             name: generateName(),
